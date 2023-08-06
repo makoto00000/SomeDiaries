da4e872afdc74aeda189f58618205fae2ceaff26
makoto hata <makotohata0429@gmail.com>
2023-08-06 10:53:32 +0900

    📝 背景画像,色設定,borderCSSを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 
         'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'diary-bg': "url('/diarybg.png')",
       },
     },
     colors: {
@@ -18,6 +19,9 @@ module.exports = {
       'subColor' : '#3F8495',
       'accentColor' : '#E6C241',
       'fontColor' : '#333333',
+      'white' : '#ffffff',
+      'gray' : '#C9C9C9',
+      'borderColor' : '#707070',
     },
   },
   plugins: [],
